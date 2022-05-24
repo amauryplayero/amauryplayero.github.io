30b6113b4dce1ccae145df5b009b43b71f04db66
Amaury <amaury.lopezdiaz@gmail.com>
2022-05-24 00:18:10 -0400

    segundo y tercer trimestre sirven en la tercera tab pero it skips certain indexes for some reason.

--- src/Components/Promedios.js
+++ src/Components/Promedios.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import {useState, useEffect} from 'react'
+
 import XLSX_ZAHL_PAYLOAD from 'https://cdn.sheetjs.com/xlsx-0.18.7/package/dist/xlsx.zahl.mjs';
 import * as XLSX from 'xlsx'
 import '../App.css';
+
+
 
 export default function Promedios() {
 
@@ -11,45 +14,20 @@ export default function Promedios() {
     const [promediosDeSegundo, setPromediosDeSegundo] = useState()
     const [promediosDeTercero, setPromediosDeTercero] = useState()
     const [nombrePromedio, setNombrePromedio] = useState()
+    const [nombrePromedioDeSegundo, setNombrePromedioDeSegundo] = useState()
+    const [nombrePromedioDePrimero, setNombrePromedioDePrimero] = useState()
+    useEffect(() => {
+        // Runs ONCE after initial rendering
+        if(promediosDeSegundo!==undefined){
+      createPromedioDeSegundo()
+     
 
- 
+        }else if(promediosDeTercero!==undefined){
+            anadirPromediosToSheet()
+        }
+      }, [promediosDeSegundo, nombrePromedio]);
+    
 
-    const createExcel = (file) =>{  
-        const reader = new FileReader();
-        reader.onload = (evt) => {
-         
-            const bstr = evt.target.result;
-            let body = {
-              fileEncoded:bstr
-            }
-      
-            const wb = XLSX.read(bstr, { type: "binary" });
-            
-            
-            
-        };
-        reader.readAsBinaryString(file);
-        
-    }
-
-    // const handleChange = (e) =>{
-    //     const [file] = e.target.files
-    //     const reader = new FileReader();
-    //     reader.onload = (evt) => {
-         
-    //         const bstr = evt.target.result;
-    //         let body = {
-    //           fileEncoded:bstr
-    //         }
-      
-    //         const wb = XLSX.read(bstr, { type: "binary" });
-    //         setExcelFile(wb)
-            
-            
-    //       };
-    //       reader.readAsBinaryString(file);
-
-    // }
 
     const handlePrimerAno = (e) =>{
 
@@ -69,13 +47,155 @@ export default function Promedios() {
         reader.readAsBinaryString(file);
 
     }
-    // console.log(promediosDePrimero.Sheets["Table 1"])
-  
+ 
     const handleSegundoAno = (e) =>{
-        
+        const [file] = e.target.files
+        const reader = new FileReader();
+        reader.onload = (evt) => {
+         
+            const bstr = evt.target.result;
+            let body = {
+              fileEncoded:bstr
+            }
+
+            const wb = XLSX.read(bstr, { type: "binary" });
+            setPromediosDeSegundo(wb)
+            
+        };
+        reader.readAsBinaryString(file);
+       
     }
 
+
+        
+
+    const createPromedioDePrimero = () =>{
+        // console.log(promediosDeSegundo)
+        let entries = Object.entries(promediosDePrimero.Sheets['Table 1'])
+        let filteredEntriesThatContainProm = entries.filter(entry=>{
+            return entry[1].v==='PROMEDIO FINAL DE GRADO'
+        })
+        let filteredEntriesThatContainNombre = entries.filter(entry=>{
+            let value = entry[1].v
+
+            return value==="NOMBRE DEL ALUMNO\r\nPRIMER APELLIDO        SEGUNDO APELLIDO     NOMBRE(S)"
+        })
+
+
+
+        // where U5 the 5 goes down to where it actually has a number so we gotta filter the entries to when a number
+
+        let numeroInicial =6
+        // mappea de las celdas donde se encuentran prom y nom para eliminar el numero y solamente 
+        let celdasDeProm = filteredEntriesThatContainProm.map((e, i)=>{
+        
+            return e[0]
+            //  EG. 'N5'
+        })
+        let celdasDeNom = filteredEntriesThatContainNombre.map((e, i)=>{
+       
+            return e[0]
+            //  EG. 'N5'
+        })
+
+
+        let letraOnlyProm = celdasDeProm.map(e=>{
+            return e.replace(/[0-9]/g, '')
+        })
+
+        let letraOnlyNom = celdasDeNom.map(e=>{
+            return e.replace(/[0-9]/g, '')
+        })
+
+       
+        // Then we loop through the thingys to create array of data that then needs to be injected into tercero's file
+        let nombresPromediosArr = []
+        for (let i = 0; i<200; i++){
+            let letraNombre = letraOnlyNom[0]
+            let letraPromedio = letraOnlyProm[0]
+            let archivo = promediosDePrimero.Sheets['Table 1']
+            
+            if(archivo[`${letraNombre+i}`]===undefined){
+                // console.log('hai')
+            }else {
+                nombresPromediosArr.push([archivo[`${letraNombre+i}`].v, parseFloat(archivo[`${letraPromedio+i}`].v).toFixed(1)])
+            
+            }
+               
+            
+        }
+
+        setNombrePromedioDePrimero([...nombresPromediosArr])
+        // console.log(nombrePromedioDeSegundo)
+    }
+
+
+    const createPromedioDeSegundo = () =>{
+        // console.log(promediosDeSegundo)
+        let entries = Object.entries(promediosDeSegundo.Sheets['Table 1'])
+        let filteredEntriesThatContainProm = entries.filter(entry=>{
+            return entry[1].v==='PROMEDIO FINAL DE GRADO'
+        })
+        let filteredEntriesThatContainNombre = entries.filter(entry=>{
+            let value = entry[1].v
+
+            return value==="NOMBRE DEL ALUMNO\r\nPRIMER APELLIDO        SEGUNDO APELLIDO     NOMBRE(S)"
+        })
+
+
+
+        // where U5 the 5 goes down to where it actually has a number so we gotta filter the entries to when a number
+
+        let numeroInicial =6
+        // mappea de las celdas donde se encuentran prom y nom para eliminar el numero y solamente 
+        let celdasDeProm = filteredEntriesThatContainProm.map((e, i)=>{
+        
+            return e[0]
+            //  EG. 'N5'
+        })
+        let celdasDeNom = filteredEntriesThatContainNombre.map((e, i)=>{
+       
+            return e[0]
+            //  EG. 'N5'
+        })
+
+
+        let letraOnlyProm = celdasDeProm.map(e=>{
+            return e.replace(/[0-9]/g, '')
+        })
+
+        let letraOnlyNom = celdasDeNom.map(e=>{
+            return e.replace(/[0-9]/g, '')
+        })
+
+       
+        // Then we loop through the thingys to create array of data that then needs to be injected into tercero's file
+        let nombresPromediosArr = []
+        for (let i = 0; i<200; i++){
+            let letraNombre = letraOnlyNom[0]
+            let letraPromedio = letraOnlyProm[0]
+            let archivo = promediosDeSegundo.Sheets['Table 1']
+            
+            if(archivo[`${letraNombre+i}`]===undefined){
+                // console.log('hai')
+            }else {
+                nombresPromediosArr.push([archivo[`${letraNombre+i}`].v, parseFloat(archivo[`${letraPromedio+i}`].v).toFixed(1)])
+            
+            }
+               
+            
+        }
+
+        setNombrePromedioDeSegundo([...nombresPromediosArr])
+        // console.log(nombrePromedioDeSegundo)
+    }
+    
+
+    
+
+    // create workbook from grado into state EX. promedios de Tercero
     const handleTercerAno = (e) =>{
+        
         const [file] = e.target.files
         const reader = new FileReader();
         reader.onload = (evt) => {
@@ -93,13 +213,28 @@ export default function Promedios() {
         
     }
 
-    const createPromedios = () =>{
+    const addEquationToCells = () =>{
         let promediosKey = Object.keys(promediosDeTercero.Sheets)[1]
+        let entries = Object.entries(promediosDeTercero.Sheets[promediosKey])
+        let filteredEntriesThatContainNombre = entries.filter(entry=>{
+            return entry[1].v==='NOMBRE DEL ALUMNO'
+        })
+
         
+
+    }
+
+    
+    const createPromediosDeTercero = () =>{
         let promediosTrimestre1 = Object.keys(promediosDeTercero.Sheets)[0]
+        let promediosKey = Object.keys(promediosDeTercero.Sheets)[1]
+        let terceraPestana = Object.keys(promediosDeTercero.Sheets)[2]
         let object = promediosDeTercero.Sheets[promediosTrimestre1]
         let entries = Object.entries(promediosDeTercero.Sheets[promediosTrimestre1])
-        // array of arrays. inside contains the object
+        
+      
+
+        // find the entries where name of student and promedio are from the trimestres section
         let filteredEntriesThatContainNombre = entries.filter(entry=>{
             return entry[1].v==='NOMBRE DEL ALUMNO'
         })
@@ -107,13 +242,15 @@ export default function Promedios() {
             return entry[1].v==='PROM.'
         })
 
-
         
+
+      
+
         let segundaTab = Object.entries(promediosDeTercero.Sheets[promediosKey])
 
         
         let numeroInicial 
-        
+        // mappea de las celdas donde se encuentran prom y nom para eliminar el numero y solamente 
         let celdasDeProm = filteredEntriesThatContainProm.map((e, i)=>{
             numeroInicial = parseInt(e[0].replace(/\D/g, ""))
             numeroInicial+=1
@@ -126,7 +263,7 @@ export default function Promedios() {
             return e[0]
             //  EG. 'N5'
         })
-
+     
         let letraOnlyProm = celdasDeProm.map(e=>{
             return e.replace(/[0-9]/g, '')
         })
@@ -134,82 +271,119 @@ export default function Promedios() {
         let letraOnlyNom = celdasDeNom.map(e=>{
             return e.replace(/[0-9]/g, '')
         })
-    
-        
-        let letrasDeSegundaTab = ['C', 'D', 'E']
+
 
         let archivoSegundaPestana = promediosDeTercero.Sheets[promediosKey]
         let archivoPrimeraPestana = promediosDeTercero.Sheets[promediosTrimestre1]
-        console.log(archivoSegundaPestana)
+        // console.log(archivoSegundaPestana)
        
-        let nombres = []
+        let nombresPromediosArr = []
         for (let i = 0; i<letraOnlyNom.length; i++){
-            for(let n = numeroInicial; n<48; n++){
+            for(let n = numeroInicial; n<60; n++){
             
             if(archivoPrimeraPestana[`${letraOnlyNom[i]+n}`]===undefined){
-                console.log('hai')
+                // console.log('hai')
             }else {
-                nombres.push([archivoPrimeraPestana[`${letraOnlyNom[i]+n}`].v, archivoPrimeraPestana[`${letraOnlyProm[i]+n}`].v])
+                nombresPromediosArr.push([archivoPrimeraPestana[`${letraOnlyNom[i]+n}`].v, parseFloat(archivoPrimeraPestana[`${letraOnlyProm[i]+n}`].v).toFixed(1)])
             
             }
                
             }
         }
-        
-        console.log(nombres)
-        setNombrePromedio([...nombres])
+        setNombrePromedio([...nombresPromediosArr])
         console.log(nombrePromedio)
-        // Now find the names and match them on the segunda pestana -----------------------
-        
+        console.log(promediosDeTercero)
 
-        // entry[0] va en 1Trim, entry[1] en la segunda etc
-        // for(let i = 0; i<nombrePromedio; i++){
-        //     for(let n =0; n<45; n++){
-        //         if(nombrePromedio[i][0]===archivoSegundaPestana[`B${n}`].v){
-        //             console.log(nombrePromedio[i])
-        //         }
-        //     }
-        // }
-
-        // nombrePromedio.map(entry=>{
-        //     archivoSegundaPestana['C'] = entry
-        //     =entry[1]
-
-        // })
-        
+    
     }    
-
+// console.log(promediosDeTercero)
     const anadirPromediosToSheet = () =>{
-        let promediosKey = Object.keys(promediosDeTercero.Sheets)[1]
+        // 
         let promediosTrimestre1 = Object.keys(promediosDeTercero.Sheets)[0]
+        let promediosKey = Object.keys(promediosDeTercero.Sheets)[1]
+        let terceraPestana = Object.keys(promediosDeTercero.Sheets)[2]
         let archivoSegundaPestana = promediosDeTercero.Sheets[promediosKey]
         let archivoPrimeraPestana = promediosDeTercero.Sheets[promediosTrimestre1]
+        let archivoTerceraPestana = promediosDeTercero.Sheets[terceraPestana]
         let entriesDeSegundaPestana = Object.entries(archivoSegundaPestana)
+       
+
+        let archivo = promediosDeTercero.Sheets[terceraPestana]
+
+        // let filteredEntryThatContainsNombreEnTercera = terceraPestana.filter(entry=>{
+        //     return entry[1].v==='NOMBRE DEL ALUMNO'
+        // })
         
         for(let i = 0; i<entriesDeSegundaPestana.length; i++){
 
             let filtered = nombrePromedio.filter(e=>{
                 return entriesDeSegundaPestana[i][1].v === e[0]
-                  
-             })
+            })
 
-         if(filtered.length===0){
-            
-        }else{
-            
-            console.log(filtered)
+            if(filtered.length===0){
+                
+            }else{
+                let cellNo = entriesDeSegundaPestana[i][0].substring(1)
+                // console.log(cellNo)
+                
+                archivoSegundaPestana['C'+`${cellNo}`].v=filtered[0][1]
+                archivoSegundaPestana['D'+`${cellNo}`].v=filtered[1][1]
+                archivoSegundaPestana['E'+`${cellNo}`].v=filtered[2][1]
+                // console.log(filtered)
+              
+
             }
            
         }
-
-
-        console.log(entriesDeSegundaPestana)
+        
+// loop where the third sheets is injected with values. 
+        // let celdaDePrimero = filteredEntryThatContainsPrimero.substring(1)
         
 
-        // nombrePromedio.map(e=>{
-        //     archivoSegundaPestana
-        // })
+        let entriesDeTerceraPestana = Object.entries(archivoTerceraPestana)
+        console.log(archivoTerceraPestana)
+    //    console.log(nombrePromedio)
+       console.log(entriesDeTerceraPestana)
 
+        for(let i = 0; i<entriesDeTerceraPestana.length; i++){
+            // console.log(entriesDeTerceraPestana[i])
+            // let celdaDeNombre = filteredEntryThatContainsNombreEnTercera.substring(1)
+             let celdaDeNombre = 'D'
+
+            let calificacionDeSegundo = nombrePromedioDeSegundo.filter(e=>{
+                
+                return entriesDeTerceraPestana[i][1].v === e[0]
+            })
+            let calificacionDeTercero = nombrePromedio.filter(e=>{
+                return entriesDeSegundaPestana[i][1].v === e[0]
+            })
+
+            let calificacionDePrimero = nombrePromedioDePrimero.filter(e=>{
+                return entriesDeTerceraPestana[i][1].v === e[0]
+            })
+
+            console.log(calificacionDeSegundo)
+            if(calificacionDeSegundo.length===0){
+                console.log(entriesDeTerceraPestana[i])
+            }else{
+                let cellNo = entriesDeTerceraPestana[i][0].substring(1)
+                console.log(cellNo)
+                
+                archivoTerceraPestana['D'+`${cellNo}`].v=calificacionDeSegundo[0][1]
+                archivoTerceraPestana['E'+`${cellNo}`].v=calificacionDeTercero[0][1]
+                archivoTerceraPestana['C'+`${cellNo}`].v=calificacionDePrimero[0][1]
+                
+              
+
+            }
+           
+
+
+        }
+        console.log(promediosDeTercero)
+        XLSX.writeFile(promediosDeTercero,'TEST DELETE.xlsx', { ignoreEC: true, bookType: 'xlsx'})
+
+ 
     }
   
     async function changeValue (){
@@ -232,18 +406,22 @@ export default function Promedios() {
     </div>
 
     <h3>2do Año</h3>
-    <input type="file" onChange={(e)=>handleSegundoAno(e)} />
+    <input type="file" onChange={(e)=>{handleSegundoAno(e)
+                                        }} />
+    <button onClick={()=>createPromedioDeSegundo()}>SEGUNDO GRADO</button>
     
    
     
     <h3>3er Año</h3>
-    <input type="file" onChange={(e)=>handleTercerAno(e)} />
+    <input type="file" onChange={(e)=>{handleTercerAno(e)
+                                    }} />
     
    
     <br></br>
     <br></br>
-    <button onClick={()=>createPromedios()}>get promedios</button>
-    <button onClick={()=>anadirPromediosToSheet()}>anadir promedios to excel</button>
+    {/* <button onClick={()=>createPromedios()}>get promedios</button> */}
+    <button onClick={()=>{createPromediosDeTercero()
+        anadirPromediosToSheet()}}>anadir promedios to excel</button>
    
     </>
   )
